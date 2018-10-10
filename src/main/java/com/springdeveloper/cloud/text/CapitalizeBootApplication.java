c004bdac6ca50ac1fc5c13139de5603f192684f5
trisberg <trisberg@pivotal.io>
2018-10-10 13:14:14 -0400

    Use Flux

--- src/main/java/com/springdeveloper/cloud/text/CapitalizeBootApplication.java
+++ src/main/java/com/springdeveloper/cloud/text/CapitalizeBootApplication.java
@@ -3,6 +3,7 @@ package com.springdeveloper.cloud.text;
 import java.util.function.Function;
 
 import org.apache.commons.text.WordUtils;
+import reactor.core.publisher.Flux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,8 +13,8 @@ import org.springframework.context.annotation.Bean;
 public class CapitalizeBootApplication {
 
 	@Bean
-	public Function<String, String> capitalize() {
-		return s -> WordUtils.capitalizeFully(s);
+	public Function<Flux<String>, Flux<String>> capitalize() {
+		return in -> in.map(s -> WordUtils.capitalizeFully(s));
 	}
 
 	public static void main(String[] args) {
